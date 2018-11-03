3963e07d3003cd01788f0062a9c505d0c10b83b4
Sergej Schmal <s.shmall@hotmail.com>
2018-11-03 05:17:01 +0100

    Add project files.

---
+++ PatientSignatureTerminal/App.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Specialized;
+using System.Windows;
+using System.Windows.Controls;
+using CommonServiceLocator;
+using Prism.Events;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using Prism.Unity;
+
+namespace PatientSignatureTerminal
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.Register(typeof(IEventAggregator), typeof(EventAggregator));
+            containerRegistry.Register<IRegionManager, RegionManager>();
+            //containerRegistry.RegisterInstance(_logManager);
+            containerRegistry.RegisterSingleton<IViewModelAsyncComponent, ViewModelAsyncComponent>();
+
+        }
+
+        protected override Window CreateShell()
+        {
+            return ServiceLocator.Current.GetInstance<MainWindow>();
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            base.ConfigureModuleCatalog(moduleCatalog);
+            var serviceModuleType = typeof(ServiceModule);
+            moduleCatalog.AddModule(
+                new ModuleInfo
+                {
+                    ModuleName = serviceModuleType.Name,
+                    ModuleType = serviceModuleType.AssemblyQualifiedName
+                });
+        }
+
+        protected override void ConfigureRegionAdapterMappings(RegionAdapterMappings regionAdapterMappings)
+        {
+            base.ConfigureRegionAdapterMappings(regionAdapterMappings);
+
+            regionAdapterMappings.RegisterMapping(typeof(StackPanel), Container.Resolve<StackPanelRegionAdapter>());
+            regionAdapterMappings.RegisterMapping(typeof(Grid), Container.Resolve<GridRegionAdapter>());
+        }
+    }
+
+    public class StackPanelRegionAdapter : RegionAdapterBase<StackPanel>
+    {
+        public StackPanelRegionAdapter(IRegionBehaviorFactory factory)
+            : base(factory)
+        {
+
+        }
+
+        protected override void Adapt(IRegion region, StackPanel regionTarget)
+        {
+            region.Views.CollectionChanged += (s, e) =>
+            {
+                if (e.Action == NotifyCollectionChangedAction.Add)
+                {
+                    foreach (FrameworkElement element in e.NewItems)
+                    {
+                        regionTarget.Children.Add(element);
+                    }
+                }
+
+                //implement remove
+            };
+        }
+
+        protected override IRegion CreateRegion()
+        {
+            return new AllActiveRegion();
+        }
+    }
+
+    public class GridRegionAdapter : RegionAdapterBase<Grid>
+    {
+        public GridRegionAdapter(IRegionBehaviorFactory factory)
+            : base(factory)
+        {}
+
+        protected override void Adapt(IRegion region, Grid regionTarget)
+        {
+            region.Views.CollectionChanged += (s, e) =>
+            {
+                if (e.Action == NotifyCollectionChangedAction.Add)
+                {
+                    foreach (FrameworkElement element in e.NewItems)
+                    {
+                        regionTarget.Children.Add(element);
+                    }
+                }
+
+                //implement remove
+            };
+        }
+
+        protected override IRegion CreateRegion()
+        {
+            return new AllActiveRegion();
+        }
+    }
+
+    public class ViewModelAsyncComponent : IViewModelAsyncComponent
+    {
+    }
+
+    public interface IViewModelAsyncComponent
+    {
+    }
+
+    public class ServiceModule : IModule
+    {
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+        }
+    }
+}
